18457c992f7739269126585e92cc9a38416267f7
Gaan <cf4856@gmail.com>
2023-05-28 12:45:01 +0900

    test

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('wiki/<str:title>/', views.entry_details, name="wiki_entry"),
-    #path('friend/<str:names>/', views.names, name="my_name")
 ]
