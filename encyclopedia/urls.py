5578f032a550fb73e090b7f97aa21e84791f8681
chrisfinns <cf4856@gmail.com>
2023-05-30 16:50:57 +0900

    search function - WIP

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
 
-
+app_name = 'encyclopedia'
 urlpatterns = [
     path("", views.index, name="index"),
     path('wiki/<str:title>/', views.entry_details, name="wiki_entry"),
+    path('search/', views.search, name="query")
+
 ]
