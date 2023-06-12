f5d02a772b3096bf050ea50138bec26fc0b08671
chrisfinns <cf4856@gmail.com>
2023-06-12 13:52:34 +0900

    New Page Funtion complete

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django import forms
 from . import util
+from django.contrib import messages
+from django.urls import reverse
 
 
 class searchForm(forms.Form):
     query = forms.CharField(label="Search")
 
+
+""" def search(request):
+    query = request.GET.get('q', '').lower()
+    search_results = []
+    entries = util.list_entries()
+    
+   
+    for entry in entries:
+        if query == entry.lower():
+            search_results.append(query)
+            entry_page = "wiki/" + str(query.lower())
+            print(entry_page)
+            return redirect(entry_page)
+      
+
+    return render(request, "encyclopedia/search.html", {
+        "results": search_results,
+        
+    })
+ """
 
 def search(request):
     query = request.GET.get('q', '')
@@ -15,18 +37,19 @@ def search(request):
     
    
     for entry in entries:
-        if query == entry:
-            search_results.append(query)
-      
+        if query.lower() in entry.lower():
+            search_results.append(entry)
+            
+            if query.lower() == entry.lower():
+                entry_page = "wiki/" + str(query.lower())
+                
+                return redirect(entry_page)
 
     return render(request, "encyclopedia/search.html", {
         "results": search_results,
-        
     })
- 
+
 def new_page(request):
-    
-    
     return render(request, "encyclopedia/new_page.html")
     
 
@@ -44,6 +67,35 @@ def entry_details(request, title):
         {'title': title, 'content': content})
 
 
-#def search(request, searchText):
-#  return none
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="title")
+    content = forms.CharField(widget=forms.Textarea(attrs={'name':'body', 'rows':3,'cols':5}))
 
+def new_page(request):
+    filenames = util.list_entries()
+    if request.method =="POST":    
+        form = NewPageForm(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+
+            filenames = [x.lower() for x in filenames]
+            
+            if title.lower() not in filenames:
+                util.save_entry(title, content)
+                entry_page = "wiki/" + str(title.lower())
+                
+                return redirect(entry_page)
+            
+            else:
+                messages.error(request, 'Entry Exisist')
+
+        else:
+            return render(request,"encyclopedia/new_page.html", {
+                "form": form
+            })
+    
+    return render(request, "encyclopedia/new_page.html", {
+        "form": NewPageForm()
+    })
---
+++ entries/asdflkjal;skdjf.md
@@ -0,0 +1,1 @@
+asldkjfadls;f
