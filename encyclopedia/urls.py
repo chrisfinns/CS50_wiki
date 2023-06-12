a54eaa67472371266b227de8184eda148d45836d
chrisfinns <cf4856@gmail.com>
2023-06-12 14:05:51 +0900

    Completed Random Page Function

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('wiki/<str:title>/', views.entry_details, name="wiki_entry"),
     path('search', views.search, name="search"),
-    path('new_page', views.new_page, name="new_page")
+    path('new_page', views.new_page, name="new_page"),
+    path('random', views.random_page, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,31 +4,12 @@ from django import forms
 from . import util
 from django.contrib import messages
 from django.urls import reverse
+import random
 
 
 class searchForm(forms.Form):
     query = forms.CharField(label="Search")
 
-
-""" def search(request):
-    query = request.GET.get('q', '').lower()
-    search_results = []
-    entries = util.list_entries()
-    
-   
-    for entry in entries:
-        if query == entry.lower():
-            search_results.append(query)
-            entry_page = "wiki/" + str(query.lower())
-            print(entry_page)
-            return redirect(entry_page)
-      
-
-    return render(request, "encyclopedia/search.html", {
-        "results": search_results,
-        
-    })
- """
 
 def search(request):
     query = request.GET.get('q', '')
@@ -99,3 +80,13 @@ def new_page(request):
     return render(request, "encyclopedia/new_page.html", {
         "form": NewPageForm()
     })
+
+
+def random_page(request):
+    entries = util.list_entries()
+    randomPage = random.choice(entries)
+    print(f"{randomPage}")
+
+
+    entry_page = "wiki/" + str(randomPage.lower())
+    return redirect(entry_page)
